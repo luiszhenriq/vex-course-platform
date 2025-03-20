a25115adca92a6617baec9cd34cc562c8b64ab3e
Luis Henrique <luishenrique2859@hotmail.com>
2025-03-20 16:28:48 -0300

    first commit

---
+++ src/main/java/br/com/luis/vex/VexApplication.java
@@ -0,0 +1,13 @@
+package br.com.luis.vex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luis/vex/VexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luis.vex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
