3336b406fffb2c5ff87071b00f15282fb819270f
Luis Henrique <luishenrique2859@hotmail.com>
2025-04-05 13:04:43 -0300

    feat: add regex by taxId and cellphone

--- src/main/java/br/com/luis/vex/dto/user/UserRegisterDTO.java
+++ src/main/java/br/com/luis/vex/dto/user/UserRegisterDTO.java
@@ -2,6 +2,7 @@ package br.com.luis.vex.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record UserRegisterDTO(
 
@@ -13,9 +14,17 @@ public record UserRegisterDTO(
         String email,
 
         @NotBlank(message = "Este campo não pode ser vazio")
+        @Pattern(
+                regexp = "^\\d{8,11}$",
+                message = "Deve conter apenas números (mínimo 8 e máximo 11 dígitos)"
+        )
         String cellphone,
 
         @NotBlank(message = "Este campo não pode ser vazio")
+        @Pattern(
+                regexp = "^\\d{11}$",
+                message = "Deve conter apenas números (com 11 digítos)"
+        )
         String taxId,
 
         @NotBlank(message = "Este campo não pode ser vazio")
